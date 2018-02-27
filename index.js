a8b6f1adcacafbac3ec133056416867fc14698c2
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-02-27 14:39:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ bot.on('message', message => {
     let sender = message.author; // This variable takes the message, and finds who the author is.
     let cont = message.content.slice(prefix.length).split(" "); // This variable slices off the prefix, then puts the rest in an array based off the spaces
     let args = cont.slice(1); // This slices off the command in cont, only leaving the arguments.
-    let myRole = message.guild.roles.get("264410914592129025");	
-    let myRole = message.guild.roles.find("name", "Moderators");
 	
     // Commands
     if(message.member.roles.has(role.id)) {
