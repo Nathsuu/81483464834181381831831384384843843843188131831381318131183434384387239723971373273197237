48f452acf4cd781d8bb9e062e331d3c455b9208c
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-02-28 21:09:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,16 +6,21 @@ const bot = new Discord.Client();
 const prefix = 'v!'; // This is the prefix, you can change it to whatever you want.
 
 // Listener Event: Runs whenever a message is received.
+
 bot.on('message', message => {
 
     // Variables - Variables make it easy to call things, since it requires less typing.
     let msg = message.content.toUpperCase(); // This variable takes the message, and turns it all into uppercase so it isn't case sensitive.
     let sender = message.author; // This variable takes the message, and finds who the author is.
     let cont = message.content.slice(prefix.length).split(" "); // This variable slices off the prefix, then puts the rest in an array based off the spaces
-    let args = cont.slice(1); // This slices off the command in cont, only leaving the arguments.
-	
-    var input = message.content.toUpperCase();	
-    
+    let args = cont.slice(1); // This slices off the command in cont, only leaving the arguments.	
+    var input = message.content.toUpperCase();
+    // Ping
+    if (msg === prefix + 'PING') { // This checks if msg (the message but in all caps), is the same as the prefix + the command in all caps.
+
+        // Now, let's send a response.
+        message.delete(); // This 'sends' the message to the channel the message was in. You can change what is in the message to whatever you want.
+    }
     if (message.content === prefix + "help"){
         var help_embed = new Discord.RichEmbed()
         .setColor('#E2FB00')
@@ -116,7 +121,7 @@ bot.on('message',message => {
 	m += 'Plusieurs commandes s offre à vous pour contacter le support : \`v!youtube\`,\`v!support\` ou \`v!bot\` et \`v!invite\`.';
 	message.author.sendMessage(m).catch(console.log);
 }
-
+});
 
 // Listener Event: Runs whenever the bot sends a ready event (when it first starts for example)
 bot.on('ready', () => {
@@ -135,6 +140,8 @@ bot.on("guildMemberAdd", member => {
     member.addRole(role)
     	
 })
+
+
 
 
 
