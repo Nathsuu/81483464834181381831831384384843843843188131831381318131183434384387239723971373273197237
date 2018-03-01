ce5049dd5608f4318bc79c80fbb85e676a316817
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-03-01 11:10:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,6 @@ bot.on('message', message => {
         .addField(":clipboard: Utilitaire", "\`help\`, \`bot\`, \`youtube\`, \`invite\`")
         .addField(":sweat_drops: NSFW", "\`girl\`")
         .addField("-", "Total serveurs: \`Soon\`, Total utilisateurs: \`Soon\`")
-	.setDescription(`Response time: ${m.createdTimestamp - message.createdTimestamp}ms`)
         .setAuthor("Pong!", "https://cdn.discordapp.com/attachments/347288279357456387/348647441815306241/ping.png")	
         .addField(":eye: Support", "[[Serveur Support]](https://discord.gg/qfYACVE)", true)	
         .setFooter("VacBot | Vaction | by WinDino | Demande par " + message.author.tag, message.author.displayAvatarURL)
