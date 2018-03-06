9f5e2844424186cffbf0f8929a9acaba323a9952
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-03-06 16:09:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,45 +19,7 @@ bot.login(process.env.TOKEN);
 
 
 bot.on('message', message => {
-    let command = message.content.split(" ")[0];
-    const args = message.content.slice(prefix.length).split(/ +/);
-    command = args.shift().toLowerCase();
- 
-    if (command === "kick") {
-        let modRole = message.guild.roles.find("name", "Test");
-        if(!message.member.roles.has(modRole.id)) {
-            return message.reply("Tu n'as pas la permission de faire cette commande.").catch(console.error);
-        }
-        if(message.mentions.users.size === 0) {
-            return message.reply("Merci de mentionner l'utilisateur à expluser.").catch(console.error);
-        }
-        let kickMember = message.guild.member(message.mentions.users.first());
-        if(!kickMember) {
-            return message.reply("Cet utilisateur est introuvable ou impossible à expulser.")
-        }
-        if(!message.guild.member(bot.user).hasPermission("KICK_MEMBERS")) {
-            return message.reply("Je n'ai pas la permission KICK_MEMBERS pour faire ceci.").catch(console.error);
-        }
-        kickMember.kick().then(member => {
-            message.reply(`${member.user.username} a été expulsé avec succès.`).catch(console.error);
-            message.guild.channels.find("name", "general").send(`**${member.user.username} a été expulsé du discord par **${message.author.username}**`)
-        }).catch(console.error)
-       
-    }
- 
-    if (command === "ban") {
-        let modRole = message.guild.roles.find("name", "Test");
-        if(!message.member.roles.has(modRole.id)) {
-            return message.reply("Tu n'as pas la permission de faire cette commande.").catch(console.error);
-        }
-        const member = message.mentions.members.first();
-        if (!member) return message.reply("Merci de mentionner l'utilisateur à bannir.");
-        member.ban().then(member => {
-            message.reply(`${member.user.username} a été banni avec succès.`).catch(console.error);
-            message.guild.channels.find("name", "general").send(`**${member.user.username}** a été banni du discord par **${message.author.username}**`)
-        }).catch(console.error)	    
-	    
-		
+
     let msg = message.content.toUpperCase(); // This variable takes the message, and turns it all into uppercase so it isn't case sensitive.
     let sender = message.author; // This variable takes the message, and finds who the author is.
     let cont = message.content.slice(prefix.length).split(" ")[0]; // This variable slices off the prefix, then puts the rest in an array based off the spaces
