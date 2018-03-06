b29bd893d4391f69dbf152315206933d98894b19
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-03-06 16:06:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -55,8 +55,7 @@ bot.on('message', message => {
         member.ban().then(member => {
             message.reply(`${member.user.username} a été banni avec succès.`).catch(console.error);
             message.guild.channels.find("name", "general").send(`**${member.user.username}** a été banni du discord par **${message.author.username}**`)
-        }).catch(console.error)
-}	    
+        }).catch(console.error)	    
 	    
 		
     let msg = message.content.toUpperCase(); // This variable takes the message, and turns it all into uppercase so it isn't case sensitive.
