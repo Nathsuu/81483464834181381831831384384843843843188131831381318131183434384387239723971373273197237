dad72fc5da4645155dd87ee9db17d867217ef784
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-03-06 17:33:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -52,13 +52,13 @@ bot.on('message', message => {
         }
         const member = message.mentions.members.first();
         if (!member) return message.reply("Merci de mentionner l'utilisateur à bannir.");
-        member.ban().then(member => {
-            message.reply(`${member.user.username} a été banni avec succès.`).catch(console.error);
-            message.guild.channels.find("name", "vchannel").send(`**${member.user.username}** a été banni du discord par **${message.author.username}** :boot:`)
-	var help_embed = new Discord.RichEmbed()
-            .setColor('#E2FB00')
+	    var help_embed = new Discord.RichEmbed()
+	    .setColor('#E2FB00')
 	    .setAuthor("Vaction | VacBot", "https://image.noelshack.com/fichiers/2018/09/4/1519899146-17332945-138497173341771-651541625360613376-n-copie.jpg")
             .addField("test1", "test")	
+        member.ban().then(member => {
+            message.reply(`${member.user.username} a été banni avec succès.`).catch(console.error);
+            message.guild.channels.find("name", "vchannel").send(`**${member.user.username}** a été banni du discord par **${message.author.username}** :boot:`)	
         }).catch(console.error)
     }
 
