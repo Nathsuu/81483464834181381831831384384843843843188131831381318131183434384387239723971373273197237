f35bca79dc64ceb4bd08d1951a65fb49d624d3c3
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-03-07 11:59:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ bot.on('message', message => {
 	}	
         kickMember.kick().then(member => {
             message.reply(`${member.user.username} a été expulsé avec succès.`).catch(console.error);
-	    message.guild.channels.find("name", "general").send(`**${member.user.username}** a été expulsé du discord par **${message.author.username}**`)
+	    message.guild.channels.find("name", "vchannel").send(`**${member.user.username}** a été expulsé du discord par **${message.author.username}**`)
 	}).catch(console.error)	
     }
        
