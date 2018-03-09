8216ea3777a04ae7fb714f0b5a7575de2ae85102
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-03-09 10:10:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ bot.on('message', message => {
        if (!args){
        args = null;
         }
-       if(message.author.id == "282123215537569793"){
+       if(!message.member.hasPermission("ADMINISTRATOR")) return;
      
        message.channel.send(args.join(` `))
        }else{
