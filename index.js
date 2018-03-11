4d85887981562be118645a12fe567cac50e894e5
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-03-11 14:02:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -30,6 +30,29 @@ bot.on("channelDelete", async channel => {
 
 });
 
+bot.on("messageDelete",  function(message) {
+if(!message.author.bot){
+if(message.guild){
+const channel = message.guild.channels.find('name', 'vchannel');
+if(!channel) {
+return;
+}
+channel.send(`Message par ${message.member.user.username} a été supprimé de 
+${message.channel.name}: ${message.content}`);
+}}
+});
+bot.on("messageUpdate",  (message, oldMessage, newMessage) =>  {
+if(!message.author.bot){
+if(message.guild){
+const channel = message.guild.channels.find('name', 'vchannel');
+if(!channel) {
+return;
+}
+channel.send(`Message par ${message.author.username} a edité
+${message.content} à ${oldMessage}`);
+}}
+});	
+
 bot.login(process.env.TOKEN);
 
 bot.on('message', message => {
@@ -155,6 +178,45 @@ bot.on('message', message => {
     return;
   }	  
 
+if(msg.content.startsWith(prefix + 'mute')){
+if(msg.channel.type === 'dm') return;
+if(!msg.guild.member(msg.author).hasPermission('ADMINISTRATOR')){
+return msg.reply("**:x: Vous n'avez pas la permissions d'utiliser cette commande**").catch(console.error);
+}
+if(msg.mentions.users.size === 0){
+return msg.reply("**:x: Veuillez mentionner l'utilisateur que vous voulez mute**")
+}
+if(!msg.guild.member(client.user).hasPermission('ADMINISTRATOR')){
+return msg.reply("**:x: Je n'ai pas la permission `ADMINISTRATOR` pour mute cet utilisateur**").catch(console.error);
+}
+let muteMember = msg.guild.member(msg.mentions.users.first());
+if(!muteMember){
+return msg.channel.send("**:x: Cet utilisateur n'est certainement pas valide**")
+}
+msg.channel.overwritePermissions(muteMember, {SEND_MESSAGES: false}).then(member => {
+msg.channel.send(`**${member.user.username}** a bien été mute**`);
+})
+}
+if(msg.content.startsWith(prefix + 'unmute')){
+if(msg.channel.type === 'dm') return;
+if(!msg.guild.member(msg.author).hasPermission('ADMINISTRATOR')){
+return msg.reply("**:x: Vous n'avez pas la permissions d'utiliser cette commande**").catch(console.error);
+}
+if(msg.mentions.users.size === 0){
+return msg.reply("**:x: Veuillez mentionner l'utilisateur que vous voulez unmute**")
+}
+if(!msg.guild.member(client.user).hasPermission('ADMINISTRATOR')){
+return msg.reply("**:x: Je n'ai pas la permission `ADMINISTRATOR` pour unmute cet utilisateur**").catch(console.error);
+}
+let unmuteMember = msg.guild.member(msg.mentions.users.first());
+if(!unmuteMember){
+return msg.channel.send("**:x: Cet utilisateur n'est certainement pas valide**")
+}
+msg.channel.overwritePermissions(unmuteMember, {SEND_MESSAGES: true}).then(member => {
+msg.channel.send(`**${member.user.username}** a bien été unmute**`);
+})
+}	
+	
   if (command === "report") {
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!rUser) return message.channel.send(":x: Vous n'avez mentionné aucun utilisateur !");
