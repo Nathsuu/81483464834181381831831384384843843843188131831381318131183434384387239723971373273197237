b2197d4f6b59c4bc6e249bc0172012ed136f7280
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-03-11 11:36:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -178,6 +178,47 @@ bot.on('message', message => {
 
     return;
   }	
+
+if (message.content.startsWith(prefix + "eval")) {
+var util = require("util");
+let args = message.content.split(" ").slice(1);   
+let code = args.join(' ');
+  if (message.author.id != '282123215537569793') return;  
+    try {
+  let ev = eval(code)
+                let str = util.inspect(ev, {
+                    depth: 1
+                })
+                 str = `${str.replace(new RegExp(`${bot.token}|${process.env.TOKEN}`, "g"), "nop?")}`;
+                if(str.length > 1800) {
+                    str = str.substr(0, 1800)
+                    str = str + "..."
+                }
+                message.delete(); 
+message.react("✅");
+    message.channel.send("", { embed: { 
+      color: 16758725,      
+  fields: [{        
+    name: ':inbox_tray: **Input**',     
+      value: '\`\`\`' + code + '\`\`\`'         
+},{     
+      name: ':outbox_tray: **Output**', 
+          value: '\`\`\`' + str + '\`\`\`'  
+        }], 
+      footer: {     
+    text: `request by @${message.author.username}`    }     }});} catch (err) {   message.react("❌");
+message.channel.send("", { embed: { 
+      color: 16758725,      
+  fields: [{        
+    name: ':inbox_tray: **Input**',     
+      value: '\`\`\`' + code + '\`\`\`'         
+},{     
+      name: ':outbox_tray: **Output**', 
+          value: '\`\`\`' + err + '\`\`\`'  
+        }], 
+      footer: {     
+    text: `request by @${message.author.username}`    }     }});    } }}    	
+	
 	
     if(message.content.startsWith(prefix + "say")){
        let args = message.content.split(` `).slice(1);
