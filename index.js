472c79bde77ab40a887e3c9502b0ed78147f48f7
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-03-14 16:12:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -340,11 +340,11 @@ message.channel.send("", { embed: {
        message.channel.sendEmbed(help_embed)
 
        }
-function random(min, max) {
-    min = Math.ceil(0);
-    max = Math.floor(3);
-    randnum = Math.floor(Math.random() * (max - min +1) + min);
-}	
+    function random(min, max) {
+        min = Math.ceil(0);
+        max = Math.floor(3);
+        randnum = Math.floor(Math.random() * (max - min +1) + min);
+        }	
     if (message.content === prefix + "e-girl"){		    
         if (randnum ==1){
 	    var help_embed = new Discord.RichEmbed()
