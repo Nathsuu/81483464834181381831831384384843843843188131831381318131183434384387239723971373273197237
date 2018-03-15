9a6998150b279311e1c7efb012e531455bcee43f
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-03-15 18:18:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -213,7 +213,7 @@ bot.on('message', message => {
   .setAuthor("🔲 | Vaction | dé")
   .setColor("#E2FB00")
   .addField("Votre pari", question)
-  .addField("Réponse", replies[result])
+  .addField("Réponse du Nombre", replies[result])
   .setFooter("VacBot | Vaction | Demande par " + message.author.tag, message.author.displayAvatarURL);
   message.channel.sendEmbed(help_embed)
   }	
