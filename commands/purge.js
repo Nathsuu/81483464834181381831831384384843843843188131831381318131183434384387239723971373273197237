9dee05a1a4c64ee4d1f55fa91bd3218c947632c4
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-03-16 20:38:49 +0100

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -6,9 +6,9 @@ if(!message.guild.member(message.author).hasPermission("MANAGE_GUILD")) return m
 
 const user = message.mentions.users.first();
  const amount = !!parseInt(message.content.split(' ')[1]) ? parseInt(message.content.split(' ')[1]) : parseInt(message.content.split(' ')[2]) 
-if (!amount) return message.reply(':x: Veuillez mentionner une limite de message'); 
+if (!amount) return message.reply(':x: Veuillez mentionner une limite de message comprise entre 3 et 100'); 
 if (!amount && !user) 
-return message.reply(':x: Veuillez mentionner une limite de message');
+return message.reply(':x: Veuillez mentionner une limite de message comprise entre 3 et 100');
 if (!user){ 
 if(isNaN(message.content.split(' ')[1]) || parseInt(message.content.split(' ')[1]) < 2 || parseInt(message.content.split(' ')[1]) > 100){
 message.channel.send(':x: Veuillez mentionner une limite de message comprise entre 2 et 100')
