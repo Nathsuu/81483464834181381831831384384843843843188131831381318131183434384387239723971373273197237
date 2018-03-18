f84cde209447fd99b79e2dd778b31ce64d9c06e7
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-03-18 17:43:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -310,7 +310,7 @@ warns(message, prefix, bot)
     if(command === "ping") {
 
     const m = await message.channel.send("Ping?");
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ping)}ms`);
     }	
 	
 if (message.content.startsWith(prefix + "eval")) {
