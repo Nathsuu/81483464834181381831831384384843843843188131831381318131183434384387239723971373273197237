77e0a771556bd1e4cf3d4542792fd17f1803ac19
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-03-19 18:15:59 +0100

    Update pussy.js

--- commands/nsfw/pussy.js
+++ commands/nsfw/pussy.js
@@ -4,7 +4,8 @@ const request = require('snekfetch');
 const fs = require("fs")
 
 exports.run = (bot, message, args) => {
-    if (!message.channel.nsfw) return message.channel.send(":x: Cette commande est executable dans un channel NSFW :underage:.")
+    if (!message.channel.nsfw) {
+        message.channel.send(":x: Cette commande est executable dans un channel NSFW :underage:.")
 
     var subreddits = [
         'pussy',
@@ -16,7 +17,7 @@ exports.run = (bot, message, args) => {
         'spreading'
     ]
     var sub = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
-
+    } else { 
     randomPuppy(sub)
             .then(url => {
                 var randomname = Math.floor(Math.random() * (99999999999999999999 - 11111111111111111111 + 0)) + 11111111111111111111;
