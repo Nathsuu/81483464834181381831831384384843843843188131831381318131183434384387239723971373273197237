44d030d686f900941d6ab49cc73500baa31f289a
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-03-19 18:33:36 +0100

    Create e-girl.js

---
+++ commands/nsfw/e-girl.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const request = require('snekfetch');
+const fs = require("fs")
+
+exports.run = (bot, message, args) => {
+    var max = 5244;
+    var min = 1000;
+    var MathRan = Math.floor(Math.random() * (max - min + 0)) + min;
+    var MathLoL = Math.round(MathRan);
+    if (!message.channel.nsfw) {
+        message.channel.send(":x: Cette commande est executable dans un channel NSFW :underage:.")
+              
+    } else {
+        
+        
+        var randomname = Math.floor(Math.random() * (99999999999999999999 - 11111111111111111111 + 0)) + 11111111111111111111;
+        request.get("http://media.obutts.ru/butts_preview/0" + MathLoL + ".jpg").then(r => {
+            fs.writeFile(`${randomname}.jpg`, r.body)
+        
+          //  message.channel.send(r.body)
+            message.channel.sendFile(r.body).then(d => {
+                fs.unlink(`./${randomname}.jpg`)
+            })
+        })
+    }
+}
