fb5d26eeb94e528d1b59e12c1bd15ad2ab893b4b
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-03-19 18:12:14 +0100

    Update pussy.js

--- commands/nsfw/pussy.js
+++ commands/nsfw/pussy.js
@@ -21,10 +21,10 @@ exports.run = (bot, message, args) => {
             .then(url => {
                 var randomname = Math.floor(Math.random() * (99999999999999999999 - 11111111111111111111 + 0)) + 11111111111111111111;
                 request.get(url).then(r => {
-                    fs.writeFile(${randomname}.jpg, r.body)
+                    fs.writeFile(`${randomname}.jpg`, r.body)
                     message.channel.sendFile(r.body).then(d => {
-                        fs.unlink(./${randomname}.jpg)
-                    })
+                        fs.unlink(`./${randomname}.jpg`)
+                })
             })
         })
 }
