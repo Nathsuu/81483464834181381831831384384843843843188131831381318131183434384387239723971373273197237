e3c8b1eae6a73851fbf5e30eab5bed474ca35075
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-03-21 15:55:38 +0100

    Update stats.js

--- commandes/stats.js
+++ commandes/stats.js
@@ -1,14 +1,21 @@
-exports.run = async(client, message, args, config, { MessageEmbed }) => {
+const Discord = require("discord.js");
 
-  const embed = new MessageEmbed()
-  .setTitle(`Etat d'${config.name}`)
+module.exports.run = async (client, message) => {
+
+ const embed = new Discord.RichEmbed()
+  .setTitle(`Info`)
   .setDescription(`Infos sur l'états de status des membre de \`${message.guild.name}\``)
-  .setThumbnail(message.guild.iconURL())
-  .setColor(config.color)
-  .setFooter(`©${config.name}`)
+  .setThumbnail(message.guild.iconURL)
+  .setColor(0xffffff)
+  .setFooter(`©`)
   .addField('Online:', "<:online:379776303743631372>"  + message.guild.members.filter(m => m.presence.status == 'online').size, true)
   .addField('Offline:', "<:offline:379776320349011968>"  + message.guild.members.filter(m => m.presence.status == 'offline').size, true)
   .addField('Dnd:', "<:dnd:379775982149566473>"  + message.guild.members.filter(m => m.presence.status == 'dnd').size, true)
   .addField('Idle:', "<:idle:379775845348409346>"  + message.guild.members.filter(m => m.presence.status == 'idle').size, true)
   message.channel.send(embed)
-};
+
+}
+
+module.exports.help = {
+  name:"member"
+}
