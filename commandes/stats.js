d0a32e54725fade3b7352bd1480bad39bf2f976b
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-03-21 16:30:32 +0100

    Update stats.js

--- commandes/stats.js
+++ commandes/stats.js
@@ -7,20 +7,20 @@ module.exports.run = async (client, message) => {
   .setThumbnail(sicon)
   .setDescription(`Infos sur l'états de status des membre de \`${message.guild.name}\``)
   .setColor("#FF9900")
-  .addField('Online:', ":large_blue_circle:"  + message.guild.members.filter(m => m.presence.status == 'online').size, true)
-  .addField('Offline:', ":white_circle:"  + message.guild.members.filter(m => m.presence.status == 'offline').size, true)
-  .addField('Dnd:', ":red_circle:"  + message.guild.members.filter(m => m.presence.status == 'dnd').size, true)
-  .addField('Idle:', ":black_circle:"  + message.guild.members.filter(m => m.presence.status == 'idle').size, true)
-	 .addBlankField() 
+  .addField('Online:', ":large_blue_circle: "  + message.guild.members.filter(m => m.presence.status == 'online').size, true)
+  .addField('Offline:', ":white_circle: "  + message.guild.members.filter(m => m.presence.status == 'offline').size, true)
+  .addField('Dnd:', ":red_circle: "  + message.guild.members.filter(m => m.presence.status == 'dnd').size, true)
+  .addField('Idle:', ":black_circle: "  + message.guild.members.filter(m => m.presence.status == 'idle').size, true)
+  .addBlankField() 
   .setTitle(`Information du Discord`)
   .addField("Nom du Discord", message.guild.name)
   .addField("Propriétaire du serveur", message.guild.owner.user.username)
   .addField("Créer le", message.guild.createdAt, true)
   .addField("Vous avez rejoint", message.member.joinedAt, true)
- 	.addBlankField()
+  .addBlankField()
   .addField(":floppy_disk: Total serveurs :", client.guilds.size)
   .addField(":floppy_disk: Total utilisateurs :", client.users.size)
- 	.addField(":floppy_disk: Utilisateurs sur le discord :", message.guild.memberCount);
+  .addField(":floppy_disk: Utilisateurs sur le discord :", message.guild.memberCount);
   message.channel.send(embed)
 
 }
