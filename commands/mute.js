b6506de00f29ef3347e83fe671b3e048f82410ec
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-03-21 17:50:04 +0100

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -2,7 +2,7 @@ function mute(message,prefix,client){
     
      if (message.content.startsWith(prefix + "mute")) {
 if (message.channel.type === "dm") return;
-if(!message.guild.member(message.author).hasPermission("MANAGE_GUILD")) return message.reply(":x: Vous n'avez pas la permission `manage-guild`.").catch(console.error);
+if(!message.guild.member(message.author).hasPermission("MANAGE_GUILD")) return message.reply(":x: Il vous faut la permission `manage-guild` pour executer cette commande.").catch(console.error);
 if(message.mentions.users.size === 0) {
   return message.channel.send(":x: Vous n'avez mentionnée aucun utilisateur !");
 }
@@ -11,10 +11,10 @@ if(!muteMember) {
   return message.channel.send(":x: Vous n'avez mentionné aucun utilisateur !");
 }
 if(!message.guild.member(client.user).hasPermission("MANAGE_GUILD")) {
-  return message.reply(":x: Je n'ai pas la permission \`MANAGE_GUILD\` pour faire ceci.").catch(console.error);
+  return message.reply(":x: Il me faut la permission `manage-guild` pour executer cette commande.").catch(console.error);
 }
 message.channel.overwritePermissions(muteMember, { SEND_MESSAGES: false }).then(member => {
-    message.channel.send(`**${muteMember.user.username}** est désormais mute dans **#${message.channel.name}** :mute:`)
+    message.channel.send(`:white_check_mark: ${muteMember.user.username}** est mute !`)
 })
 }}
     module.exports = mute
