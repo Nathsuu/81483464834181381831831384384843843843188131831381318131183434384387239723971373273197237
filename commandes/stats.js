012a013da21fa3eddcb6ee2802fae0ec0a4ed6a1
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-03-21 16:22:51 +0100

    Update stats.js

--- commandes/stats.js
+++ commandes/stats.js
@@ -1,17 +1,24 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (client, message) => {
-
+ let sicon = message.guild.iconURL;
  const embed = new Discord.RichEmbed()
-  .setTitle(`Info`)
+  .setTitle(`Stats du Serveur`)
+  .setThumbnail(sicon)
   .setDescription(`Infos sur l'états de status des membre de \`${message.guild.name}\``)
-  .setThumbnail(message.guild.iconURL)
-  .setColor(0xffffff)
-  .setFooter(`©`)
+  .setColor("#FF9900")
   .addField('Online:', ":large_blue_circle:"  + message.guild.members.filter(m => m.presence.status == 'online').size, true)
   .addField('Offline:', ":white_circle:"  + message.guild.members.filter(m => m.presence.status == 'offline').size, true)
   .addField('Dnd:', ":red_circle:"  + message.guild.members.filter(m => m.presence.status == 'dnd').size, true)
   .addField('Idle:', ":black_circle:"  + message.guild.members.filter(m => m.presence.status == 'idle').size, true)
+  .setDescription(`Information du Discord`)
+  .addField("Nom du Discord", message.guild.name)
+  .addField("Propriétaire du serveur", message.guild.owner.user.username)
+  .addField("Créer le", message.guild.createdAt, true)
+  .addField("Vous avez rejoint", message.member.joinedAt, true)
+  .addField(":floppy_disk: Total serveurs :", client.guilds.size)
+  .addField(":floppy_disk: Total utilisateurs :", client.users.size)
+ 	.addField(":floppy_disk: Utilisateurs sur le discord :", message.guild.memberCount);
   message.channel.send(embed)
 
 }
