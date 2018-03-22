24f56c7a2b5e4e9d337e7d415b15b6d556d525c4
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-03-22 20:36:16 +0100

    Update tempmute.js

--- commandes/tempmute.js
+++ commandes/tempmute.js
@@ -5,11 +5,11 @@ module.exports.run = async (bot, message, args) => {
 
   if(!message.member.hasPermission("MANAGE_GUILD")) return message.reply(":x: Il vous faut la permission `manage-guild` pour executer cette commande.");
   if(args[0] == "help"){
-    message.reply(":x: Vous n'avez pas mentionné un nombre ! Exemple : `v!tempmute @User <1s/m/h/d>`");
+    message.reply(":x: Vous n'avez pas mentionné un nombre ! Exemple : ``v!tempmute @User <1s/m/h/d>``");
     return;
   }
   let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(!tomute) return message.reply(":x: Vous n'avez pas mentionné un nombre ! Exemple : `v!tempmute @User <1s/m/h/d>");
+  if(!tomute) return message.reply(":x: Vous n'avez pas mentionné un nombre ! Exemple : ``v!tempmute @User <1s/m/h/d>``");
   if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply(":x: Vous n'avez pas la permission de faire cette commande sur lui.");
   let reason = args.slice(2).join(" ");
   if(!reason) return message.reply(":x: Il me faut une raison.");
@@ -49,13 +49,13 @@ module.exports.run = async (bot, message, args) => {
   .setDescription("TempMute")
   .setColor("#FF9900")
   .addField("Utilisateur Tempmute", tomute)
-  .addField("Tempmute par", `<@${message.author}> avec l'ID ${message.author.id}`)
+  .addField("Tempmute par", `${message.author} avec l'ID ${message.author.id}`)
     .addField("Tempsmute depuis", message.channel)
-  .addField("Temps du Mute", mutetime)
+  .addField("Temps du Tempmute", mutetime)
   .addField("Raison", reason);
 
   let incidentschannel = message.guild.channels.find(`name`, "vchannel");
-  if(!incidentschannel) return message.reply(":x:Impossible de trouver le canal \`vchannel\`.");
+  if(!incidentschannel) return message.reply(":x: Impossible de trouver le canal \`vchannel\`.");
   incidentschannel.send(muteembed);
 
   await(tomute.addRole(muterole.id));
