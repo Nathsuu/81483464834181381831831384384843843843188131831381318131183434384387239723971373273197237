f107e596c6547e3411478010fa791fa5532a0820
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-03-23 18:15:25 +0100

    Update and rename tempmute.js to mute.js

--- commandes/mute.js
+++ commandes/mute.js
@@ -5,22 +5,22 @@ module.exports.run = async (bot, message, args) => {
 
   if(!message.member.hasPermission("MANAGE_GUILD")) return message.reply(":x: Il vous faut la permission `manage-guild` pour executer cette commande.");
   if(args[0] == "help"){
-    message.reply(":x: Vous n'avez pas mentionné un nombre ! Exemple : ``v!tempmute @User <1s/m/h/d>``");
+    message.reply(":x: Vous n'avez pas mentionné un utilisateur ! Exemple : ``Exemple : ``v!mute @User Insulte``");
     return;
   }
   let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(!tomute) return message.reply(":x: Vous n'avez pas mentionné un nombre ! Exemple : ``v!tempmute @User <1s/m/h/d>``");
+  if(!tomute) return message.reply(":x: Vous n'avez pas mentionné un nombre ! Exemple : ``v!mute @User Insulte``");
   if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply(":x: Vous n'avez pas la permission de faire cette commande sur lui.");
   let reason = args.slice(2).join(" ");
   if(!reason) return message.reply(":x: Il me faut une raison.");
 
-  let muterole = message.guild.roles.find(`name`, "muted");
+  let muterole = message.guild.roles.find(`name`, "vmuted");
 
   if(!muterole){
     try{
       muterole = await message.guild.createRole({
-        name: "muted",
-        color: "#000000",
+        name: "vmuted",
+        color: "#FF9900",
         permissions:[]
       })
       message.guild.channels.forEach(async (channel, id) => {
@@ -34,40 +34,36 @@ module.exports.run = async (bot, message, args) => {
     }
   }
 
-  let mutetime = args[1];
-  if(!mutetime) return message.reply(":x: Vous n'avez pas spécifié de temps!");
-
   message.delete().catch(O_o=>{});
 
   try{
-    await tomute.send(`:exclamation: Vous venez d'être mute ${mutetime}.`)
+    await tomute.send(`:exclamation: Vous venez d'être mute sur un serveur m'utilisant.`)
   }catch(e){
-    message.channel.send(`Un utilisateur a été mute, mais sont DM est verrouillés. Il sera mute pour ${mutetime}`)
+    message.channel.send(`L'utilisateur a été mute, mais sont DM est verrouillés.`)
   }
 
   let muteembed = new Discord.RichEmbed()
-  .setDescription("TempMute - Encore SOON")
+  .setDescription("Mute")
   .setColor("#FF9900")
-  .addField("Utilisateur Tempmute", tomute)
-  .addField("Tempmute par", `${message.author}`)
-    .addField("Tempmute depuis", message.channel)
-  .addField("Temps du Tempmute", mutetime)
+  .addField("Utilisateur Mute", tomute)
+  .addField("Mute par", `${message.author}`)
+  .addField("Mute depuis", message.channel)
   .addField("Raison", reason);
 
-  let incidentschannel = message.guild.channels.find(`name`, "vchannel");
-  if(!incidentschannel) return message.reply(":x: Impossible de trouver le canal \`vchannel\`.");
-  incidentschannel.send(muteembed);
+  let vchannelchannel = message.guild.channels.find(`name`, "vchannel");
+  if(!vchannelchannel) return message.reply(":x: Impossible de trouver le canal \`vchannel\`.");
+  vchannelchannel.send(muteembed);
 
   await(tomute.addRole(muterole.id));
 
   setTimeout(function(){
     tomute.removeRole(muterole.id);
-    message.channel.send(`:white_check_mark: <@${tomute.id}> a bien été tempmute !`);
+    message.channel.send(`:white_check_mark: <@${tomute.id}> a bien été mute !`);
   }, ms(mutetime));
 
 
 }
 
 module.exports.help = {
-  name: "tempmute"
+  name: "mute"
 }
