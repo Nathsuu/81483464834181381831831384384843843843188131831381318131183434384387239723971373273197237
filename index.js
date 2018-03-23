4c9a4bae06156b82718ade77750fa735ee8763f3
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-03-23 19:12:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -167,7 +167,7 @@ unmute(message, prefix, client)
     .setTimestamp()	  
 	  
     let requestchannel = message.guild.channels.get("423552696411357204");
-    if(!requestchannel) return message.channel.send(":x: Impossible de trouver le canal avec l'id ``423552696411357204``.");	  
+    if(!requestchannel) return message.channel.send(":x: Impossible de trouver le canal avec l'id ``423552696411357204`` L'iD du cannal est un channel du support du bot.");	  
 
     message.delete().catch(O_o=>{});
     requestchannel.send(requestEmbed);
