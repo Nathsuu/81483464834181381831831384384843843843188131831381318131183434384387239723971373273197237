9f407c76c21705780cf2450a408f1775a1207852
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-03-26 20:52:12 +0200

    Update stats.js

--- commandes/stats.js
+++ commandes/stats.js
@@ -24,7 +24,7 @@ module.exports.run = async (client, message) => {
   .addField("Propriétaire du Serveur", message.guild.owner.user.username)
   .addField("Serveur créer le", message.guild.createdAt, true)
   .addField("Vous avez rejoint le", message.member.joinedAt, true)
-  .addField('Roles', message.guild.members.get(getvalueof.id).roles.array().map(g => "```" + g.name + "```").join(' ❱ ')") 
+  .addField('Roles', message.guild.members.get(getvalueof.id).roles.array().map(g => "```" + g.name + "```").join(' ❱ ') 
   .addBlankField()
   .addField(":floppy_disk: Total serveurs :", client.guilds.size)
   .addField(":floppy_disk: Total utilisateurs :", client.users.size)
