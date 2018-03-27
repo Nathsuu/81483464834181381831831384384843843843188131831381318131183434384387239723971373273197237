4d88732a2e5db03537c06cbce18bbe65ce4712e1
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-03-27 18:45:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 let prefix = setprefix[message.guild.id].setprefix;
 
-const prefix = 'v!'; 
 
 client.on('ready', () => {
   setInterval(function(){
