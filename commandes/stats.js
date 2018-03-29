96068fbd3faac21bf1aa069323d7ad7fc4a019e6
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-03-29 17:54:30 +0200

    Update stats.js

--- commandes/stats.js
+++ commandes/stats.js
@@ -27,7 +27,7 @@ module.exports.run = async (client, message) => {
   .addField('Vos roles actuels', message.guild.members.get(getvalueof.id).roles.array().map(g => "" + g.name + "").join(', ')) 
   .addBlankField() 
   .addField(":floppy_disk: Utilisateurs sur le Discord :", message.guild.memberCount)
-  .addField(":floppy_disk: Channels sur le Discord :", message.guild.channelCount)
+  .addField(":floppy_disk: Channels sur le Discord :", message.guild.channelsCount)
   .addBlankField() 
   .addField(":floppy_disk: Total Serveurs :", client.guilds.size)
   .addField(":floppy_disk: Total Utilisateurs :", client.users.size)
