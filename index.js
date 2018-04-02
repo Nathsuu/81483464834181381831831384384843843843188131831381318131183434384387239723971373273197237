1e1243f09f879e114163f3b03186100024a76dfa
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-04-02 18:31:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -517,6 +517,45 @@ message.channel.send("", { embed: {
 
     }
   } 
+	
+  if (message.content.startsWith(prefix + "fakehacker")) {	
+	if(!message.member.hasPermission("ADMINISTRATOR")) {	
+            return message.reply(":comet: Vous n'avez pas la permission de faire cette commande.").catch(console.error);
+        }
+  	message.channel.send("LE DISCORD VIENS DE SE FAIRE HACKé !", {
+        tts: true
+        })    		    
+	message.channel.send('**__[HACKING]__** **LANCEMENT DU PROCESSEUR**')
+	message.channel.send('**__[HACKING]__** **486846868486464868468464864846846846868468448468686864846864846846446665765476547657465476547657**')
+	message.channel.send('**__[HACKING]__** **PIRATAGE EN COURS......**') 
+	message.channel.send('**__[HACKING]__** **SUPPRESSION DES DONNEES**') 
+	message.channel.send('**__[HACKING]__** **10%**')
+	message.channel.send('**__[HACKING]__** **20%**')
+	message.channel.send('**__[HACKING]__** **30%**')
+	message.channel.send('**__[HACKING]__** **40%**')
+	message.channel.send('**__[HACKING]__** **50%**')
+	message.channel.send("50 % RESANTS", {    
+        tts: true
+        }) 	    
+	message.channel.send('**__[HACKING]__** **MODIFICATION DU DISCORD.....**')
+	message.channel.send('**__[HACKING]__** **.........................**')
+	message.channel.send('**__[HACKING]__** **LANCEMENT DE VAC.EXE**')
+	message.channel.send('**__[HACKING]__** **60%**')
+	message.channel.send('**__[HACKING]__** **70%**')
+	message.channel.send('**__[HACKING]__** **80%**')
+	message.channel.send('**__[HACKING]__** **90%**')
+	message.channel.send('**__[HACKING]__** **100%**')
+	message.channel.send('**__[HACKING]__** **LE PIRATAGE EST UN SUCCES !**')
+	message.channel.send("LE DISCORD A BIEN ETE HACKé !", {
+        tts: true
+        })	  
+  
+    message.delete().catch(O_o=>{});	  
+
+    console.log("Commande v!fakehacker demandé !");	  	  	  
+
+    return;
+  }	
 	  
 
     let msg = message.content.toUpperCase(); // This variable takes the message, and turns it all into uppercase so it isn't case sensitive.
@@ -524,7 +563,7 @@ message.channel.send("", { embed: {
     let cont = message.content.slice(prefix.length).split(" ")[0]; // This variable slices off the prefix, then puts the rest in an array based off the spaces	
     var input = message.content.toUpperCase();
 
-    if (message.content == ("v!fakehacker")){
+    if (message.content == ("v!fakehackerr")){
 	message.channel.send("LE DISCORD VIENS DE SE FAIRE HACKé !", {
         tts: true
         })    		    
@@ -537,6 +576,9 @@ message.channel.send("", { embed: {
 	message.channel.send('**__[HACKING]__** **30%**')
 	message.channel.send('**__[HACKING]__** **40%**')
 	message.channel.send('**__[HACKING]__** **50%**')
+	message.channel.send("50 % RESANTS", {    
+        tts: true
+        }) 	    
 	message.channel.send('**__[HACKING]__** **MODIFICATION DU DISCORD.....**')
 	message.channel.send('**__[HACKING]__** **.........................**')
 	message.channel.send('**__[HACKING]__** **LANCEMENT DE VAC.EXE**')
