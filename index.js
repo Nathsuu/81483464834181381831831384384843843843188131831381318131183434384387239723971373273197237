ee2e5ca9193070765ef98990bac217c646bd8d0f
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-04-04 12:25:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -425,13 +425,15 @@ if (message.content.startsWith(prefix + "pfc")) {
   console.log("Commande v!triggered demandé !");	  
   }
 	
-  if (message.content === 'B!lienbot') {
+  if (message.content === 'v!image404') {
   const embed = new Discord.RichEmbed()
   .setTitle("")
-  .setAuthor("Lien des bots")
-  .setColor(0x01FECC)
-  .setDescription("\n404Bot \n**https://discordapp.com/oauth2/authorize?client_id=424913354776248320&scope=bot&permissions=8** \nErrorRaid404 \n**https://discordapp.com/oauth2/authorize?client_id=426007879787479040&scope=bot&permissions=8**")
-  .setImage(`https://cdn.discordapp.com/attachments/395910811379564555/427452249216778246/404.png`)
+  .setAuthor("Image 404")
+  .setColor("#FF9900")
+  .addField("Test", "Test", true)
+  .setImage(`https://cdn.discordapp.com/attachments/395910811379564555/427452249216778246/404.png`, true)
+  .addField("Test", "Test", true)
+  .setImage(`https://cdn.discordapp.com/attachments/395910811379564555/427452249216778246/404.png`, true)  
   .setFooter("")
   message.channel.send({embed});
   }	
