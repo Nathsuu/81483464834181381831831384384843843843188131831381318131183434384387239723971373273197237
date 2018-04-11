f018ff90add5a34a70a925dd4b8aab9f162a1c9a
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-04-11 17:06:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -364,7 +364,8 @@ if (message.content.startsWith(prefix + "pfc")) {
   let result = Math.floor((Math.random() * replies.length));
   let question = args.slice(0).join(" ");
 	
-  message.channel.send('*Discord Résolver...*').then(m => m.edit(`:comet: L'adresse ip de `+ iUser +` est ` +replies[result]));	  
+  message.channel.send('*Discord Résolver...*').then(m => m.edit(`:comet: L'adresse ip de `+ iUser +` est ` +replies[result]));
+  console.log("Commande v!ip demandé !");	  
   }	
 	
   if (message.content.startsWith(prefix + "flip")) {	
