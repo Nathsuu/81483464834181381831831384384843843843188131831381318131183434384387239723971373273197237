bc3f0d7513df6e89ea3caf87c157833f2dec1840
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-04-30 12:10:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ client.on('ready', () => {
 client.login(process.env.TOKEN);
 
 client.on('guildCreate', (guild) => {
-    const invite = await guild.channels.first().createInvite({
+    const invite = guild.channels.first().createInvite({
       maxAge: 0
     });	
     const channel = client.guilds.get('341585907368984576').channels.get(`409708031048286208`).send('**Un Serveur viens de m ajouter !** \n '+guild.name+', **Propriétaire : **'+guild.owner.user.username+', **Nombre de membres : **'+guild.memberCount);
