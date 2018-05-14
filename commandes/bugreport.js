ad889c1f8bba67eb8f0c0a7f1bc3175234e8cabf
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-05-14 19:27:16 +0200

    Update bugreport.js

--- commandes/bugreport.js
+++ commandes/bugreport.js
@@ -10,7 +10,7 @@ try {
     }
     const bug = args.join(" ")
     if (!bug) return message.channel.send(':comet: S il vous plaît spécifier un bug !')
-    const content = clean(`**${message.author.username}**#${message.author.discriminator} (${message.author.id}) a rapporté un bug :\n${bug}\nServer: **${message.guild.name}**\nID: **${message.guild.id}**`);
+    const content = clean(`**${message.author.username}**#${message.author.discriminator} (${message.author.id}) a rapporté un bug :\n${bug}\nServeuur: **${message.guild.name}**\nID: **${message.guild.id}**`);
     const id = '445636165408784386';
     new Promise((resolve, reject) => {
       superagent.post(`https://discordapp.com/api/channels/${id}/messages`)
