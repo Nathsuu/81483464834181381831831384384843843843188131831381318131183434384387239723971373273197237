10afcdd989479c7b7efb561c4d378aaa04774fd8
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-05-14 19:08:53 +0200

    Create eval2.js

---
+++ commandes/eval2.js
@@ -0,0 +1,39 @@
+const Discord = require("discord.js");
+
+exports.run = async (client, message, args, level) => { 
+  const code = args.join(" ");
+  try {
+    const evaled = eval(code);
+    const clean = await client.clean(client, evaled);
+    if (code) {
+    const embed = new Discord.RichEmbed()
+    .setAuthor(`${message.author.tag}`, message.author.avatarURL)
+    .setColor("RANDOM")
+    .addField("Input\⬇", `${code}`)
+    .addField("Output\⬇", `\`\`\`js\n${clean}\n\`\`\``)
+    message.channel.send({embed});} else {
+      const embed = new Discord.RichEmbed()
+      .setAuthor(`${message.author.tag}`, message.author.avatarURL)
+      .setColor("RANDOM")
+      .addField("Code", `none`)
+      .addField("Output", `\`\`\`js\n${clean}\n\`\`\``)
+    message.channel.send({embed});
+    }
+  } catch (err) {
+    message.channel.send(`\`ERROR\` \`\`\`xl\n${await client.clean(client, err)}\n\`\`\``);
+  }
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: "Bot Owner"
+};
+
+exports.help = {
+  name: "eval2",
+  category: "Owner",
+  description: "Evaluates arbitrary javascript.",
+  usage: "eval [...code]"
+};
