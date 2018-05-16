1bf34556e93eeb6be6a29d108948793ef7388fba
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-05-16 20:47:09 +0200

    Update tempmute.js

--- commandes/tempmute.js
+++ commandes/tempmute.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const ms = require("ms");
 
-module.exports.run = async (bot, message, args) => {**
+module.exports.run = async (bot, message, args) => {
 
   let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if(!tomute) return message.reply(":comet: Vous n'avez pas mentionné un utilisateur ! Exemple : ``v!tempmute @User 1s/m/h/d``");
