b8e094641a7a6cf97b8777289737b820ff3e2f7b
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-05-16 14:23:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -76,8 +76,34 @@ info(message, prefix, client)
 	    message.guild.channels.find("name", "vchannel").send(`**${member.user.username}** a été expulsé du discord par **${message.author.username}**`)
 	}).catch(console.error)	
     }
-
-
+    if (message.content.startsWith(prefix + "purge2")) {
+	const user = message.mentions.users.first();
+	if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('Sorry, you don\'t have permission to delete or purge messages!')
+		.then(msg => msg.delete({
+			timeout: 10000
+		}));
+	const amount = !!parseInt(message.content.split(' ')[1]) ? parseInt(message.content.split(' ')[1]) : parseInt(message.content.split(' ')[2])
+	if (!amount) return message.channel.send('Specify an amount of messages to delete or purge!')
+		.then(msg => msg.delete({
+			timeout: 10000
+		}));
+	if (!amount && !user) return message.channel.send('Specify a user and amount, or just an amount, of messages to delete or purge!')
+		.then(msg => msg.delete({
+			timeout: 10000
+		}));
+	message.channel.messages.fetch({
+			limit: 500
+		, })
+		.then((messages) => {
+			if (user) {
+				const filterBy = user ? user.id : client.user.id;
+				messages = messages.filter(m => m.author.id === filterBy)
+					.array()
+					.slice(0, amount);
+			}
+			message.channel.bulkDelete(messages)
+				.catch(error => console.log(error.stack));
+		});
 	
   if(message.content.startsWith(prefix + "chat")) {	  
      message.delete();
