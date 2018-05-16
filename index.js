3cade1d25c1a9e2c9a964ae823a1ac1783b2af5f
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-05-16 19:36:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -686,7 +686,7 @@ message.channel.send("", { embed: {
         .addField(":gear: Espaces Configurations", "```v!setgame \nv!say \nv!channel \nv!eval \nv!logout \nv!approve-bot \nv!refuse-bot \nv!request-bot```", true)
 	.addBlankField()
 	.addField(":globe_with_meridians: Recherches", "```v!google \nv!twitter \nv!youtube```", true)
-	.addField(":sparkles: News", "```Création d'un troll... \nModification sur le v!purge en cours...```", true)	
+	.addField(":sparkles: News", "```Création d'un troll... \nModification sur le v!purge en cours... \nAjout du v!tempmute```", true)	
 	.addBlankField()	
         .addField(":notepad_spiral: Support", "[[Serveur Support]](https://discord.gg/qfYACVE)", true)
         .addField(":paperclip: Invitation du Bot", "[[Invitation]](https://discordapp.com/oauth2/authorize?client_id=417993047427776512&scope=bot&permissions=2146958583)", true)
