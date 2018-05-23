9fd8ae49b54e516ab0dbd870beca58d726b79584
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-05-23 14:06:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -513,7 +513,7 @@ if (message.content.startsWith(prefix + "pfc")) {
  if(message.content.startsWith(prefix + "tri")) {	
  const snek = require("snekfetch");
  let type = "triggered";
- let url = "${message.author.displayAvatarURL}";
+ let url = `${message.author.displayAvatarURL}`;
 
  snek.get(`http://www.triggered-api.tk/api/v2/${type}?url=${url}`).set({ Authorization: (process.env.TOKENAPI) }).then(response => {
 
