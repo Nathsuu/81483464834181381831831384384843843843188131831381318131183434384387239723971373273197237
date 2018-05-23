75eb3903b68ab19d461e51370726965b5519f561
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-05-23 17:23:48 +0200

    Update setwelcome.js

--- commandes/setwelcome.js
+++ commandes/setwelcome.js
@@ -5,7 +5,9 @@ module.exports.run = async (client, message, args) => {
  db.set(`wmsg_${message.guild.id}`, args.join(" ")).then(i => {
  
    message.channel.send({embed: { description: `Welcome Message was changed to **${i}**` }});
- 
+   message.channel.send(`Welcome Message was changed to **${i}**`);
+  
+  
  });
  
 }
