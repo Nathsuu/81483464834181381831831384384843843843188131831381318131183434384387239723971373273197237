5216ff86dd95b1826609c9a532332d89152ae0d0
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-05-23 22:37:52 +0200

    Update setchannel.js

--- commandes/setchannel.js
+++ commandes/setchannel.js
@@ -4,7 +4,7 @@ module.exports.run = async (client, message, args) => {
  
  let channel = message.mentions.channels.first()
  if(!channel) return message.channel.send(":comet: Vous n'avez mentionné aucun channel ! Exemple/Usage : \`v!setchannel #welcome\`")
- 
+ if(!message.member.hasPermission("MANAGE_GUILD")) return message.reply(":comet: Vous n'avez pas la permission ``MANAGE_GUILD`` pour faire cette commande.");
  db.set(`wchannel_${message.guild.id}`, channel.id).then(i => {
  
    message.channel.send(`Le channel des welcomes/leaves ont été changé en :\n\n<#${i}>`);
