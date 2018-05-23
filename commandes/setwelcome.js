57f8c08d5c501523d75035a6efd167d8e87bd59e
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-05-23 16:03:13 +0200

    Create setwelcome.js

---
+++ commandes/setwelcome.js
@@ -0,0 +1,15 @@
+const db = require("quick.db");
+
+module.exports.run = async (client, message, args) => {
+ 
+ db.set(`wmsg_${message.guild.id}`, args.join(" ")).then(i => {
+ 
+   message.channel.send({embed: { description: `Welcome Message was changed to **${i}**` }});
+ 
+ });
+ 
+}
+
+module.exports.help = {
+  name: "setwelcome"
+}
