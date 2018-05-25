6b86135bda1104f93ef94e7c1dfc688e77c5348b
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-05-25 22:48:50 +0200

    Update createinvite.js

--- commandes/createinvite.js
+++ commandes/createinvite.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 exports.run = async (anko, message, args, color) => {
   if (!message.member.hasPermission("CREATE_INSTANT_INVITE")) return;
   message.channel.createInvite({maxAge: 0}).then(invite => {
-  message.reply("tiens : ${invite}, par contre tu as intérêt d'inviter des personnes !");
+  message.reply(`tiens : ${invite}, par contre tu as intérêt d'inviter des personnes !`);
   });
 }
 
