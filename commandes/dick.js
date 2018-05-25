304d38df79669cfd549dd39f4c9e7bd8fe259e72
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-05-25 19:12:47 +0200

    Create dick.js

---
+++ commandes/dick.js
@@ -0,0 +1,4 @@
+const randomPuppy = require('random-puppy');const request = require('snekfetch');const fs = require("fs") exports.run = (client, message, args) => { if (!message.channel.nsfw) return message.channel.send("Cette commande n'est exécutable que dans un channel NSFW :underage:") var subreddits = [ 'dickpic' ] var sub = subreddits[Math.round(Math.random() * (subreddits.length - 1))]; randomPuppy(sub) .then(url => { request.get(url).then(r => { fs.writeFile(`dick.jpg`, r.body) message.channel.sendFile(r.body) fs.unlink(`./dick.jpg`) }) })
+module.exports.help = {
+  name: "dick"
+}
