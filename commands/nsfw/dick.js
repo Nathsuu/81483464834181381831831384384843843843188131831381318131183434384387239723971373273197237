4e568f9accdd64479dc37ec6f3ee4a8d1fc080bd
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-05-25 19:09:12 +0200

    Update dick.js

--- commands/nsfw/dick.js
+++ commands/nsfw/dick.js
@@ -1,9 +1,1 @@
-const randomPuppy = require('random-puppy');
-const request = require('snekfetch');
-const fs = require("fs");
-
-exports.run = (client, message, args) => {
-if (!message.channel.nsfw) return message.channel.send("Cette commande n'est exécutable que dans un channel NSFW :underage:") 
-var subreddits = [ 'dickpic' ] 
-var sub = subreddits[Math.round(Math.random() * (subreddits.length - 1))]; randomPuppy(sub) .then(url => { request.get(url).then(r => { fs.writeFile(`dick.jpg`, r.body) 
-message.channel.sendFile(r.body) fs.unlink(`./dick.jpg`) }) })
+const randomPuppy = require('random-puppy');const request = require('snekfetch');const fs = require("fs") exports.run = (client, message, args) => { if (!message.channel.nsfw) return message.channel.send("Cette commande n'est exécutable que dans un channel NSFW :underage:") var subreddits = [ 'dickpic' ] var sub = subreddits[Math.round(Math.random() * (subreddits.length - 1))]; randomPuppy(sub) .then(url => { request.get(url).then(r => { fs.writeFile(`dick.jpg`, r.body) message.channel.sendFile(r.body) fs.unlink(`./dick.jpg`) }) })
