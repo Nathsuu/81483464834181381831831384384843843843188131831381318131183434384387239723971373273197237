8e699610d55a16fdd26bbde620f838977ef6e957
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-06-03 12:00:55 +0200

    Create setpartners.js

---
+++ commandes/setpartners.js
@@ -0,0 +1,18 @@
+const db = require("quick.db");
+
+module.exports.run = async (client, message, args) => {
+ 
+ let Pchannel = message.mentions.channels.first()
+ if(!Pchannel) return message.channel.send(":comet: Vous n'avez mentionné aucun channel ! Exemple/Usage : \`v!setpartners #partners\`")
+ if(!message.member.hasPermission("MANAGE_GUILD")) return message.reply(":comet: Vous n'avez pas la permission ``MANAGE_GUILD`` pour faire cette commande.");
+ db.set(`wPchannel_${message.guild.id}`, Pchannel.id).then(i => {
+ 
+   message.channel.send(`Le channel des setpartners ont été changé en :\n\n<#${i}>`);
+   
+ });
+ 
+}
+
+module.exports.help = {
+  name: "setpartners"
+}
