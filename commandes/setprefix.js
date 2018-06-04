b83ee32eda860dbcb41dd6864ff489a7d447164f
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-06-04 19:02:27 +0200

    Create setprefix.js

---
+++ commandes/setprefix.js
@@ -0,0 +1,36 @@
+ let prefix;
+ 
+ let fetch = await db.fetch(`prefix_${message.guild.id}`);
+ if(fetch === null) prefix = '&'
+ else prefix = fetch
+ 
+ 
+ 
+ 
+ 
+ 
+const db = require("quick.db");
+
+module.exports.run = async (client, message, args) => {
+ 
+ let prefix;
+ let fetch = await db.fetch(`prefix_${message.guild.id}`);
+ if(fetch === null) prefix = '&'
+ else prefix = fetch
+ 
+ if(!args.join(" ")) return message.channel.send(`Mon préfixe actuel est ${prefix}`)
+ if(!args.slice(1).join(" ")) return message.channel.send(`Mon préfixe actuel est ${prefix}`)
+ 
+ if(args[0] === 'set') {
+  
+   let nPrefix = args.slice(1).join(" ");
+   
+   db.set(`prefix_${message.guild.id}`, nPrefix).then(i => {
+   
+     message.channel.send(`Le prefix sur ce serveur est désormais ${i}`)
+   
+   });
+   
+ }
+   
+}
