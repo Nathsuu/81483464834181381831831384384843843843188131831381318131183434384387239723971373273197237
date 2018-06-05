0d27f9427939e61727760eaa2d14e684e1409e02
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-06-05 18:46:26 +0200

    Update clear.js

--- commandes/clear.js
+++ commandes/clear.js
@@ -1,15 +1,15 @@
 exports.run = (client, message, args, tools) => {
 
   
-  if(!message.member.hasPermission("MANAGE_GUILD")) return message.reply(":comet: Vous n'avez pas la permission ``MANAGE_GUILD`` pour faire cette commande.");
+  if(!message.member.hasPermission("MANAGE_GUILD")) return message.reply("Vous n'avez pas la permission ``MANAGE_GUILD`` pour faire cette commande.");
 
-  if (isNaN(args[0])) return message.channel.send(":comet: Vous n'avez pas mentionné de nombre ! Exemple/Usage : ``v!clear 50``");
+  if (isNaN(args[0])) return message.channel.send("Vous n'avez pas mentionné de nombre ! Exemple/Usage : ``v!clear 50``");
   
-  if (args[0] > 100) return message.channel.send(":comet: Vous n'avez pas fourni un nombre inférieur à 100 ! Exemple/Usage : ``v!clear 50``");
+  if (args[0] > 100) return message.channel.send("Vous n'avez pas fourni un nombre inférieur à 100 ! Exemple/Usage : ``v!clear 50``");
 
 
   message.channel.bulkDelete(args[0])
-    .then(messages => message.channel.send(`:comet: Vous venez de supprimer \`${messages.size}/${args[0]}\` messages ! :ok_hand:`).then(msg => msg.delete({
+    .then(messages => message.channel.send(`Vous venez de supprimer \`${messages.size}/${args[0]}\` messages ! :ok_hand:`).then(msg => msg.delete({
       timeout: 10000
     }))) 
 
