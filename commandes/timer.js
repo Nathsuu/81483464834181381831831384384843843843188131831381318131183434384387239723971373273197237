33bf165edc77c4229f8baf02d539516aa7bdf658
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-06-05 19:03:59 +0200

    Update timer.js

--- commandes/timer.js
+++ commandes/timer.js
@@ -13,7 +13,7 @@ if(args[0] <= 0){
   return message.channel.send("Vous n'avez pas mentionné de temps ! Exemple/Usage : ``v!timer 50s/m/h``");
 }
 
-message.channel.send(":white_check_mark: La minuterie a été définie pour : " + `${ms(ms(Timer), {long: true})}`)
+message.channel.send("La minuterie a été définie pour : " + `${ms(ms(Timer), {long: true})}`)
 
 setTimeout(function(){
   message.channel.send(`La minuterie est terminée, cela a duré : ${ms(ms(Timer), {long: true})}`)
