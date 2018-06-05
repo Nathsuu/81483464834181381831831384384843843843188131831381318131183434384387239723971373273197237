b853d5d26ef6ff49a98e40cec755cf943be18860
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-06-05 18:49:27 +0200

    Update setwelcome.js

--- commandes/setwelcome.js
+++ commandes/setwelcome.js
@@ -1,7 +1,7 @@
 const db = require("quick.db");
 
 module.exports.run = async (client, message, args) => {
- if(!message.member.hasPermission("MANAGE_GUILD")) return message.reply(":comet: Vous n'avez pas la permission ``MANAGE_GUILD`` pour faire cette commande.");
+ if(!message.member.hasPermission("MANAGE_GUILD")) return message.reply("Vous n'avez pas la permission ``MANAGE_GUILD`` pour faire cette commande.");
  db.set(`wmsg_${message.guild.id}`, args.join(" ")).then(i => {
  
    message.channel.send(`Le message de welcome a été changé en :\n\n**${i}**`);
