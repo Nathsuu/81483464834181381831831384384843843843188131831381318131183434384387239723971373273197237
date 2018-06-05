3325ddbbfd47512b32745bce89fdccd6b33f5a0d
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-06-05 18:45:38 +0200

    Create timer.js

---
+++ commandes/timer.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+const ms = require("ms");
+
+module.exports.run = async (client, message, args, level) => {
+
+let Timer = args[0];
+
+if(!args[0]){
+  return message.channel.send("Vous n'avez pas mentionné de temps ! Exemple/Usage : ``v!timer 50s/m/h``");
+}
+
+if(args[0] <= 0){
+  return message.channel.send("Vous n'avez pas mentionné de temps ! Exemple/Usage : ``v!timer 50s/m/h``");
+}
+
+message.channel.send(":white_check_mark: La minuterie a été définie pour : " + `${ms(ms(Timer), {long: true})}`)
+
+setTimeout(function(){
+  message.channel.send(`La minuterie est terminée, cela a duré : ${ms(ms(Timer), {long: true})}` + message.author.toString())
+
+}, ms(Timer));
+}
+module.exports.help = {
+  name: "timer"
+}
