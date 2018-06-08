5387ad7542dd24e0dbee6730ad1a7cc6f6e57a09
WindoPvP <36110475+WindoPvP@users.noreply.github.com>
2018-06-08 19:41:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -49,11 +49,10 @@ client.on('guildMemberAdd', async member => {
   let username = `${member.user.username}`;
   let description = "Bienvenue+sur+le+serveur+!";
   let image = `${member.user.avatarURL}`;	
-  let backgroundimage = "https://i.ytimg.com/vi/LQnBTecpuX0/maxresdefault.jpg";			
-
-  snek.get(`https://www.triggered-api.tk/welcome?pseudo=${username}&description=${description}&avatarurl=${image}&background=${backgroundimage}`).then(response => {
+  let backgroundimage = "https://i.ytimg.com/vi/LQnBTecpuX0/maxresdefault.jpg";	
+	
   var dC= member.guild.channels.find("name", "testing-welcome");
-  dC.channel.send('Vaction - **Welcome**',{
+  dC.channel.send(`https://www.triggered-api.tk/welcome?pseudo=${username}&description=${description}&avatarurl=${image}&background=${backgroundimage}`),{
         files:[{
           attachment: response.body,
           name: 'welcome.png'
